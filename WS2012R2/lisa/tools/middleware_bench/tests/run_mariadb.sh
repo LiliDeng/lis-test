50af2c221965b158c9c111f1575bbe6b1cd7b633
Lili Deng <lildeng@microsoft.com>
2020-06-24 18:19:45 +0800

    Fix mariadb

--- WS2012R2/lisa/tools/middleware_bench/tests/run_mariadb.sh
+++ WS2012R2/lisa/tools/middleware_bench/tests/run_mariadb.sh
@@ -113,13 +113,13 @@ ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo chown -R mysql:adm ${
 ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo service mysql start" >> ${LOG_FILE}
 sleep 30
 
-ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo mysql -e \"GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'${client_ip}' IDENTIFIED BY 'lisapassword' WITH GRANT OPTION;\"" >> ${LOG_FILE}
+ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo mysql -e \"GRANT ALL PRIVILEGES ON *.* TO '${USER}'@'%' IDENTIFIED BY 'lisapassword' WITH GRANT OPTION;\"" >> ${LOG_FILE}
 ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo mysql -e \"DROP DATABASE sbtest;\"" >> ${LOG_FILE}
 ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo mysql -e \"CREATE DATABASE sbtest;\"" >> ${LOG_FILE}
 ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo mysql -e \"SET GLOBAL max_connections = 5000;\"" >> ${LOG_FILE}
 ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo mysql -e \"FLUSH PRIVILEGES;\"" >> ${LOG_FILE}
 
-sudo sysbench --test=oltp_path --mysql-host=${SERVER} --mysql-user=${USER} --mysql-password=lisapassword --mysql-db=sbtest --oltp-table-size=100000000 prepare >> ${LOG_FILE}
+sudo sysbench --test=${oltp_path} --mysql-host=${SERVER} --mysql-user=${USER} --mysql-password=lisapassword --mysql-db=sbtest --oltp-table-size=100000000 prepare >> ${LOG_FILE}
 
 function run_mariadb ()
 {
@@ -138,7 +138,7 @@ function run_mariadb ()
     vmstat 1 2>&1 > /tmp/mariadb/${threads}.vmstat.netio.log &
     mpstat -P ALL 1 2>&1 > /tmp/mariadb/${threads}.mpstat.cpu.log &
 
-    sudo sysbench --test=oltp_path --mysql-host=${SERVER} --mysql-user=${USER} --mysql-password=lisapassword --mysql-db=sbtest --max-time=300 --oltp-test-mode=complex --mysql-table-engine=innodb --oltp-read-only=off --max-requests=100000000 --num-threads=${threads} run > /tmp/mariadb/${threads}.sysbench.mariadb.run.log
+    sudo sysbench --test=${oltp_path} --mysql-host=${SERVER} --mysql-user=${USER} --mysql-password=lisapassword --mysql-db=sbtest --max-time=300 --oltp-test-mode=complex --mysql-table-engine=innodb --oltp-read-only=off --max-requests=100000000 --num-threads=${threads} run > /tmp/mariadb/${threads}.sysbench.mariadb.run.log
 
     ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo pkill -f sar"
     ssh -T -o StrictHostKeyChecking=no ${USER}@${SERVER} "sudo pkill -f iostat"
@@ -158,7 +158,7 @@ do
     run_mariadb ${threads}
 done
 
-sudo sysbench --test=oltp_path --mysql-host=${SERVER} --mysql-user=${USER} --mysql-password=lisapassword --mysql-db=sbtest cleanup >> ${LOG_FILE}
+sudo sysbench --test=${oltp_path} --mysql-host=${SERVER} --mysql-user=${USER} --mysql-password=lisapassword --mysql-db=sbtest cleanup >> ${LOG_FILE}
 
 LogMsg "Kernel Version : `uname -r`"
 LogMsg "Guest OS : ${distro}"
